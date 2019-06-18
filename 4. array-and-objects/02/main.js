eaa13cf1e460938c2bc2e7ede61a24b5281c44ae
Aman Sharma <sharmaman4419@gmail.com>
2019-06-18 23:02:54 +0530

    add minor changes

---
+++ 4. array-and-objects/02/main.js
@@ -0,0 +1,28 @@
+var gunter = {
+    name: "Gunter",
+    origin: "Adventure Time",
+    canFly: false,
+    sayHello: function () {
+      console.log("QUACK!!!");
+    }
+  };
+  
+  var ramon = {
+    name: "Ramón",
+    origin: "Happy Feet",
+    canFly: true,
+    sayHello: function () {
+      console.log("Estoy encantado de conocerle.");
+    }
+  };
+  
+  var fred = {
+    name: "Fred",
+    origin: "Sitting Ducks",
+    canFly: false,
+    sayHello: function () {
+      console.log("Hi there!");
+    }
+  };
+  // 
+  let penguien = [Gunter,Ramon,Fred];
