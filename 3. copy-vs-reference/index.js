715c34306ffddbb6c65a491be0365e5347279b69
Aman Sharma <sharmaman4419@gmail.com>
2019-06-18 17:33:52 +0530

    solve some exercises

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -4,6 +4,7 @@ var x = 10;
 var y = "abc";
 var a = x;
 var b = y;
+// values x=10 y="abc" a=10 b= "abc"
 
 // Value of x, y, a, b
 var x = 10;
@@ -12,29 +13,38 @@ var a = x;
 var b = y;
 a = 5;
 b = "def";
+//x=10 y="5defc" a=10 b="5defc"
+
+//x=10 y="abc" a=10 b="abc"
 
 // Value of arr & arrCopy
 var arr = [1];
 var arrCopy = arr;
 arr.push(2);
+//arr = [1] arrCopy = (2) [1,2]
 
 // Value of obj
 var obj = { name: "ryan" };
 obj = { surname: "florance" };
+//obj = { surname: "florance" };
 
 // What's the output.
 var arr = ["Hi"];
 var arr2 = arr;
 console.log(arr === arr2);
+//output is true;
 
 // What's the output.
 var arr1 = ["Hi!"];
 var arr2 = ["Hi!"];
 console.log(arr1 === arr2);
 console.log(arr == arr2);
+//output of 1st is true;
+//output of 2nd is false;
 
 //What's the output
 console.log([10] === [10]);
+//false
 
 // What's the output?
 
@@ -54,6 +64,8 @@ var personObj1 = {
 var personObj2 = personDetails(personObj1);
 console.log(personObj1); // -> ?
 console.log(personObj2); // -> ?
+//name: "Alex", age: 25}
+ {name: "John", age: 50}
 
 // Guess the output
 var oldArray = [];
@@ -61,6 +73,7 @@ var object = {};
 object.newArray = oldArray;
 oldArray.push(10);
 console.log(object.newArray === oldArray);
+//output = true
 
 // Guess the output
 var a = 5;
@@ -68,6 +81,7 @@ var b = a;
 a = 10;
 console.log(a);
 console.log(b);
+// output 10 and 5
 
 // What's the output?
 var a = {};
@@ -75,6 +89,7 @@ var b = a;
 a.a = 1;
 console.log(a);
 console.log(b);
+//{a: 1} {a:1}
 
 // What's the output.
 var a = [];
@@ -83,6 +98,8 @@ a.push(1);
 console.log(a); // [1]
 console.log(b); // [1]
 console.log(a === b);
+
+//[1] [1] true
 
 // Clone the object person in clone
 var person = {
@@ -94,4 +111,4 @@ var person = {
   }
 };
 
-var clone = {};
+var clone = Object.assign({}, person);
---
+++ 4. array-and-objects/01/main.js
@@ -0,0 +1,11 @@
+let mypenguien = {
+    character:"whiteblack",
+    origin:"Whiteblack the Penguin Sees the World" , 
+    auther: "H. A. Rey and Margret Rey" 
+};
+//mypenguien.favouritefood = ["egg","meat","fish"];
+//mypenguien.favouritefood[1];
+//let firstfavfood = "egg"
+//mypenguien.favouritefood.push("grass");
+//console.log(mypenguien.favouritefood["length"]);
+//console.log(mypenguien.favfoods[2]
