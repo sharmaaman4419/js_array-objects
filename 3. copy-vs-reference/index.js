75f46d6c9295e4252d613d0b85b1c4866ca86448
Aman Sharma <sharmaman4419@gmail.com>
2019-06-25 21:22:51 +0530

    added changes

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -4,7 +4,8 @@ var x = 10;
 var y = "abc";
 var a = x;
 var b = y;
-// values x=10 y="abc" a=10 b= "abc"
+//
+ values x=10 y="abc" a=10 b= "abc"
 
 // Value of x, y, a, b
 var x = 10;
@@ -13,7 +14,8 @@ var a = x;
 var b = y;
 a = 5;
 b = "def";
-//x=10 y="5defc" a=10 b="5defc"
+//
+x=10 y="5defc" a=10 b="5defc"
 
 //x=10 y="abc" a=10 b="abc"
 
@@ -21,30 +23,36 @@ b = "def";
 var arr = [1];
 var arrCopy = arr;
 arr.push(2);
-//arr = [1] arrCopy = (2) [1,2]
+//
+arr = [1] arrCopy = (2) [1,2]
 
 // Value of obj
 var obj = { name: "ryan" };
 obj = { surname: "florance" };
-//obj = { surname: "florance" };
+//
+obj = { surname: "florance" };
 
 // What's the output.
 var arr = ["Hi"];
 var arr2 = arr;
 console.log(arr === arr2);
-//output is true;
+//
+output is true;
 
 // What's the output.
 var arr1 = ["Hi!"];
 var arr2 = ["Hi!"];
 console.log(arr1 === arr2);
 console.log(arr == arr2);
-//output of 1st is true;
-//output of 2nd is false;
+//
+output of 1st is true;
+//
+output of 2nd is false;
 
 //What's the output
 console.log([10] === [10]);
-//false
+//
+false
 
 // What's the output?
 
@@ -64,7 +72,8 @@ var personObj1 = {
 var personObj2 = personDetails(personObj1);
 console.log(personObj1); // -> ?
 console.log(personObj2); // -> ?
-//name: "Alex", age: 25}
+//
+name: "Alex", age: 25}
  {name: "John", age: 50}
 
 // Guess the output
@@ -73,7 +82,8 @@ var object = {};
 object.newArray = oldArray;
 oldArray.push(10);
 console.log(object.newArray === oldArray);
-//output = true
+//
+output = true
 
 // Guess the output
 var a = 5;
@@ -81,7 +91,8 @@ var b = a;
 a = 10;
 console.log(a);
 console.log(b);
-// output 10 and 5
+//
+ output 10 and 5
 
 // What's the output?
 var a = {};
@@ -89,7 +100,8 @@ var b = a;
 a.a = 1;
 console.log(a);
 console.log(b);
-//{a: 1} {a:1}
+//
+{a: 1} {a:1}
 
 // What's the output.
 var a = [];
@@ -99,7 +111,8 @@ console.log(a); // [1]
 console.log(b); // [1]
 console.log(a === b);
 
-//[1] [1] true
+//
+[1] [1] true
 
 // Clone the object person in clone
 var person = {
